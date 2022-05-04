dcae348ba120e582af427886301cf9c20ba5bd33
Dimitar Stoimenov <dstoimen0@gmail.com>
2022-05-04 21:15:21 +0300

    up

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,11 +1,56 @@
+import { useEffect, useState } from 'react';
 import './Slider.css';
 import { SliderData } from './SliderData';
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
 
 function Slider() {
+    const [currentImgIndex, setCurrentImgIndex] = useState(0);
+    const length = SliderData.length;
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            nextSlide();
+        }, 13000);
+        return () => {
+            clearInterval(timer);
+        };
+    }, [currentImgIndex])
+
+    function prevSlide(e) {
+        e.stopPropagation();
+        setCurrentImgIndex(currentImgIndex === 0 ? length - 1 : currentImgIndex - 1);
+    }
+
+    const nextSlide = (e) => {
+        if (e) {
+            e.stopPropagation();
+        }
+        
+        setCurrentImgIndex(currentImgIndex === length - 1 ? 0 : currentImgIndex + 1)
+    }
+
+    if (!Array.isArray(SliderData) || SliderData.length <= 0) {
+        return null;
+    }
 
     return (
-        <div id="main-page-banner-wrapper">
-            <img className="main-page-banner-image" src={SliderData[0].image}></img>
+        <div className="main-page-banner-wrapper" onClick={() => console.log('redirect to movie desc')}>
+
+            <FaArrowAltCircleLeft className='left-arrow arrow' onClick={prevSlide} />
+            <FaArrowAltCircleRight className='right-arrow arrow' onClick={nextSlide} />
+
+            {SliderData.map((slide, index) => {
+                return (
+                    <div
+                        className={index === currentImgIndex ? 'slide active' : 'slide'}
+                        key={index}
+                    >
+                        {index === currentImgIndex && (
+                            <img className="main-page-banner-image" src={slide.image} alt="banner"></img>
+                        )}
+                    </div>
+                )
+            })}
         </div>
     );
 }
