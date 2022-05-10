89e28826d34b2c1daeeb65d657f39606d4297f31
Dimitar Stoimenov <dstoimen0@gmail.com>
2022-05-11 01:57:22 +0300

    up

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -55,6 +55,13 @@ const allMoviesList = [
         movieRating: 4.7,
         movieTags: 'asd asd2 asd3',
     },
+    {
+        _id: (Math.random() * 1000).toFixed(0),
+        movieName: "Doctor Strange",
+        posterLink: "https://xl.movieposterdb.com/22_04/2/9419884/xl_9419884_2dddcf0c.jpg",
+        movieRating: 4.7,
+        movieTags: 'asd asd2 asd3',
+    },
 ];
 
 const topMoviesList = [];
