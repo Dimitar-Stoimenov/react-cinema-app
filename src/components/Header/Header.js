e0c9d00727537111f8a376527f58ffeee6ace223
Dimitar Stoimenov <dstoimen0@gmail.com>
2022-05-11 01:12:22 +0300

    up

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,22 @@
 import './Header.css';
+import { useEffect, useState } from 'react';
+
 
 function Header() {
+
+    const [scrollPosition, setScrollPosition] = useState(0);
+    const handleScroll = () => {
+        const position = window.pageYOffset;
+        setScrollPosition(position);
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll, { passive: true });
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
     return (
         <>
@@ -8,6 +24,9 @@ function Header() {
                 <li className="nav-item" id="navLeftSide">
                     <a href="#">Program</a>
                     <a href="#">Offers</a>
+                </li>
+                <li className={scrollPosition >= 48 ? "nav-item scrolled-logo-wrapper" : "nav-item scrolled-logo-wrapper hidden"}>
+                    <a id="scrolled-logo" href="#">BEAR CINEMA</a>
                 </li>
                 {/* Logged-in users */}
                 <li className="nav-item" id="user">
@@ -20,7 +39,7 @@ function Header() {
                     <a href="#">Register</a>
                 </li> */}
             </ul>
-            <a className="nav-logo-wrapper" href="#">
+            <a className={scrollPosition < 48 ? "nav-logo-wrapper" : "nav-logo-wrapper hidden"} href="#">
                 <img id="home-logo" src='./logo.png'></img>
             </a>
         </>
