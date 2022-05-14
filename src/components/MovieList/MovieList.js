16b93aca05ab75779f3708be7ff89aea09830c7d
Dimitar Stoimenov <dstoimen0@gmail.com>
2022-05-15 02:19:16 +0300

    up

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -1,30 +1,73 @@
 import './MovieList.css';
-import { getAllMovies, getClassicMovies, getTopMovies } from '../../services/movieService';
+import { getAllMovies, getClassicMovies, getTopMovies, getFamilyMovies } from '../../services/movieService';
 import MovieCard from './MovieCard/MovieCard';
 import { useEffect, useState } from 'react';
 
 const MovieList = () => {
     const [movies, setMovies] = useState([]);
-    // set active button state
+    const [activeMovieTab, setActiveMovieTab] = useState([0]);
 
     useEffect(() => {
+        clickTopMovies();
+    }, [])
+
+    const clickTopMovies = () => {
         getTopMovies()
             .then(res => {
                 setMovies(res);
+                setActiveMovieTab(0);
             })
-    }, [movies])
+    };
+    const clickClassicMovies = () => {
+        getClassicMovies()
+            .then(res => {
+                setMovies(res);
+                setActiveMovieTab(1);
+            })
+    };
+    const clickFamilyMovies = () => {
+        getFamilyMovies()
+            .then(res => {
+                setMovies(res);
+                setActiveMovieTab(2);
+            })
+    };
+    const clickAllMovies = () => {
+        getAllMovies()
+            .then(res => {
+                setMovies(res);
+                setActiveMovieTab(3);
+            })
+    };
 
     return (
         <div className='movie-list-container'>
             <ul className="movie-tabs-container">
-                <button className="movie-tab active">Top Rated Movies</button>
-                <button className="movie-tab">Classic Movies</button>
-                <button className="movie-tab">All Movies</button>
+                <button className={activeMovieTab === 0 ? 'movie-tab active' : 'movie-tab'}
+                    key={0}
+                    onClick={clickTopMovies}>
+                    Top Rated Movies
+                </button>
+                <button className={activeMovieTab === 1 ? 'movie-tab active' : 'movie-tab'}
+                    key={1}
+                    onClick={clickClassicMovies}>
+                    Classic Movies
+                </button>
+                <button className={activeMovieTab === 2 ? 'movie-tab active' : 'movie-tab'}
+                    key={2}
+                    onClick={clickFamilyMovies}>
+                    Family Movies
+                </button>
+                <button className={activeMovieTab === 3 ? 'movie-tab active' : 'movie-tab'}
+                    key={3}
+                    onClick={clickAllMovies}>
+                    All Movies
+                </button>
             </ul>
             <ul className="movie-card-container">
                 {movies.length > 0
                     ? movies.map(x => <MovieCard key={x._id} item={x} />)
-                    : <p>No movies available yet.</p>
+                    : <p className="no-movies-yet">No movies available yet.</p>
                 }
             </ul>
         </div>
--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,23 +1,172 @@
 const allMoviesList = [
     {
-        _id: 'dsitmom-2022',
+        _id: (Math.random() * 10000).toFixed(0),
         movieName: "Doctor Strange In The Multiverse Of Madness",
         posterLink: "https://xl.movieposterdb.com/22_04/2/9419884/xl_9419884_2dddcf0c.jpg",
         movieRating: 4.7,
         movieTags: 'asd asd2 asd3',
     },
+    {
+        _id: (Math.random() * 10000).toFixed(0),
+        movieName: "Interstellar",
+        posterLink: "https://xl.movieposterdb.com/15_03/2014/816692/xl_816692_284eb9d5.jpg?v=2022-02-08%2007:13:16",
+        movieRating: 4.9,
+        movieTags: 'asd asd2',
+    },
+    {
+        _id: (Math.random() * 10000).toFixed(0),
+        movieName: "Zack Snyder's Justice League",
+        posterLink: "https://xl.movieposterdb.com/21_06/2021/12361974/xl_12361974_e6b24dc8.jpg?v=2022-05-03%2023:45:02",
+        movieRating: 4.8,
+        movieTags: 'asd asd2',
+    },
+    {
+        _id: (Math.random() * 10000).toFixed(0),
+        movieName: "Inception",
+        posterLink: "https://xl.movieposterdb.com/20_05/2010/1375666/xl_1375666_7699a7d7.jpg?v=2022-03-24%2022:50:16",
+        movieRating: 4.9,
+        movieTags: 'asd asd2 asd3',
+    },
+    {
+        _id: (Math.random() * 10000).toFixed(0),
+        movieName: "Doctor Strange In The Multiverse Of Madness",
+        posterLink: "https://xl.movieposterdb.com/22_04/2/9419884/xl_9419884_2dddcf0c.jpg",
+        movieRating: 4.7,
+        movieTags: 'asd asd2 asd3',
+    },
+    {
+        _id: (Math.random() * 10000).toFixed(0),
+        movieName: "Shrek",
+        posterLink: "https://xl.movieposterdb.com/13_04/2001/126029/xl_126029_6011e23e.jpg",
+        movieRating: 4.3,
+        movieTags: 'asd asd2',
+    },
+    {
+        _id: (Math.random() * 10000).toFixed(0),
+        movieName: "Zack Snyder's Justice League",
+        posterLink: "https://xl.movieposterdb.com/21_06/2021/12361974/xl_12361974_e6b24dc8.jpg?v=2022-05-03%2023:45:02",
+        movieRating: 4.8,
+        movieTags: 'asd asd2',
+    },
+    {
+        _id: (Math.random() * 10000).toFixed(0),
+        movieName: "Inception",
+        posterLink: "https://xl.movieposterdb.com/20_05/2010/1375666/xl_1375666_7699a7d7.jpg?v=2022-03-24%2022:50:16",
+        movieRating: 4.9,
+        movieTags: 'asd asd2 asd3',
+    },
+    {
+        _id: (Math.random() * 10000).toFixed(0),
+        movieName: "Doctor Strange In The Multiverse Of Madness",
+        posterLink: "https://xl.movieposterdb.com/22_04/2/9419884/xl_9419884_2dddcf0c.jpg",
+        movieRating: 4.7,
+        movieTags: 'asd asd2 asd3',
+    },
+    {
+        _id: (Math.random() * 10000).toFixed(0),
+        movieName: "Interstellar",
+        posterLink: "https://xl.movieposterdb.com/15_03/2014/816692/xl_816692_284eb9d5.jpg?v=2022-02-08%2007:13:16",
+        movieRating: 4.9,
+        movieTags: 'asd asd2',
+    },
+    {
+        _id: (Math.random() * 10000).toFixed(0),
+        movieName: "Zack Snyder's Justice League",
+        posterLink: "https://xl.movieposterdb.com/21_06/2021/12361974/xl_12361974_e6b24dc8.jpg?v=2022-05-03%2023:45:02",
+        movieRating: 4.8,
+        movieTags: 'asd asd2',
+    },
+    {
+        _id: (Math.random() * 10000).toFixed(0),
+        movieName: "Inception",
+        posterLink: "https://xl.movieposterdb.com/20_05/2010/1375666/xl_1375666_7699a7d7.jpg?v=2022-03-24%2022:50:16",
+        movieRating: 4.9,
+        movieTags: 'asd asd2 asd3',
+    },
 ];
-
 const topMoviesList = [
     {
-        _id: 'dsitmom-2022',
-        movieName: "Doctor Strange In The Multiverse Of Madness",
-        posterLink: "https://xl.movieposterdb.com/22_04/2/9419884/xl_9419884_2dddcf0c.jpg",
-        movieRating: 4.7,
+        _id: (Math.random() * 10000).toFixed(0),
+        movieName: "Interstellar",
+        posterLink: "https://xl.movieposterdb.com/15_03/2014/816692/xl_816692_284eb9d5.jpg?v=2022-02-08%2007:13:16",
+        movieRating: 4.9,
+        movieTags: 'asd asd2',
+    },
+    {
+        _id: (Math.random() * 10000).toFixed(0),
+        movieName: "Inception",
+        posterLink: "https://xl.movieposterdb.com/20_05/2010/1375666/xl_1375666_7699a7d7.jpg?v=2022-03-24%2022:50:16",
+        movieRating: 4.9,
+        movieTags: 'asd asd2 asd3',
+    },
+    {
+        _id: (Math.random() * 10000).toFixed(0),
+        movieName: "Zack Snyder's Justice League",
+        posterLink: "https://xl.movieposterdb.com/21_06/2021/12361974/xl_12361974_e6b24dc8.jpg?v=2022-05-03%2023:45:02",
+        movieRating: 4.8,
+        movieTags: 'asd asd2',
+    },
+    {
+        _id: (Math.random() * 10000).toFixed(0),
+        movieName: "Interstellar",
+        posterLink: "https://xl.movieposterdb.com/15_03/2014/816692/xl_816692_284eb9d5.jpg?v=2022-02-08%2007:13:16",
+        movieRating: 4.9,
+        movieTags: 'asd asd2',
+    },
+    {
+        _id: (Math.random() * 10000).toFixed(0),
+        movieName: "Inception",
+        posterLink: "https://xl.movieposterdb.com/20_05/2010/1375666/xl_1375666_7699a7d7.jpg?v=2022-03-24%2022:50:16",
+        movieRating: 4.9,
+        movieTags: 'asd asd2 asd3',
+    },
+    {
+        _id: (Math.random() * 10000).toFixed(0),
+        movieName: "Zack Snyder's Justice League",
+        posterLink: "https://xl.movieposterdb.com/21_06/2021/12361974/xl_12361974_e6b24dc8.jpg?v=2022-05-03%2023:45:02",
+        movieRating: 4.8,
+        movieTags: 'asd asd2',
+    },
+    {
+        _id: (Math.random() * 10000).toFixed(0),
+        movieName: "Interstellar",
+        posterLink: "https://xl.movieposterdb.com/15_03/2014/816692/xl_816692_284eb9d5.jpg?v=2022-02-08%2007:13:16",
+        movieRating: 4.9,
+        movieTags: 'asd asd2',
+    },
+    {
+        _id: (Math.random() * 10000).toFixed(0),
+        movieName: "Shrek",
+        posterLink: "https://xl.movieposterdb.com/13_04/2001/126029/xl_126029_6011e23e.jpg",
+        movieRating: 4.3,
+        movieTags: 'asd asd2',
+    },
+];
+const classicMoviesList = [
+    {
+        _id: (Math.random() * 10000).toFixed(0),
+        movieName: "Interstellar",
+        posterLink: "https://xl.movieposterdb.com/15_03/2014/816692/xl_816692_284eb9d5.jpg?v=2022-02-08%2007:13:16",
+        movieRating: 4.9,
+        movieTags: 'asd asd2',
+    },
+    {
+        _id: (Math.random() * 10000).toFixed(0),
+        movieName: "Inception",
+        posterLink: "https://xl.movieposterdb.com/20_05/2010/1375666/xl_1375666_7699a7d7.jpg?v=2022-03-24%2022:50:16",
+        movieRating: 4.9,
         movieTags: 'asd asd2 asd3',
     },
 ];
-const classicMoviesList = [];
+const familyMoviesList = [
+    {
+        _id: (Math.random() * 10000).toFixed(0),
+        movieName: "Shrek",
+        posterLink: "https://xl.movieposterdb.com/13_04/2001/126029/xl_126029_6011e23e.jpg",
+        movieRating: 4.3,
+        movieTags: 'asd asd2',
+    },
+];
 //fix all to actual queries;
 
 export async function getAllMovies() {
@@ -30,4 +179,8 @@ export async function getTopMovies() {
 
 export async function getClassicMovies() {
     return await classicMoviesList;
+}
+
+export async function getFamilyMovies() {
+    return await familyMoviesList;
 }
