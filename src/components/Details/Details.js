f0371f14f4414a20863b865b0f102b5ec91dfe1a
Dimitar Stoimenov <dstoimen0@gmail.com>
2022-05-16 23:07:44 +0300

    up

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -26,37 +26,37 @@ const Details = () => {
                     <div className="details-movie-report">
                         <div className="report">
                             <div className="item">
-                                <div className="name">Времетраене</div>
-                                <div className="value">94 минути</div>
+                                <div className="name">Length</div>
+                                <div className="value">{movie.length}</div>
                             </div>
                             <div className="item">
-                                <div className="name">Премиера</div>
-                                <div className="value">13 май 2022 г.</div>
+                                <div className="name">Premiere</div>
+                                <div className="value">{movie.premiere}</div>
                             </div>
                             <div className="item">
-                                <div className="name">Режисьор</div>
-                                <div className="value">Keith Thomas</div>
+                                <div className="name">Director</div>
+                                <div className="value">{movie.director}</div>
                             </div>
                             <div className="item">
-                                <div className="name">Жанр</div>
-                                <div className="value">Трилър, Фентъзи, Хорър</div>
+                                <div className="name">Genres</div>
+                                <div className="value">{movie.genres.join(', ')}</div>
                             </div>
                             <div className="item">
-                                <div className="name">Актьори</div>
-                                <div className="value">Ryan Kiera Armstrong, Zac Efron, Sydney Lemmon, Kurtwood Smith, Michael Greyeyes, Gloria Reuben</div>
+                                <div className="name">Cast</div>
+                                <div className="value">{movie.cast.join(', ')}</div>
                             </div>
                             <div className="item">
-                                <div className="name">RATING</div>
-                                <div className="value">4.8</div>
+                                <div className="name">Rating</div>
+                                <div className="value">{movie.movieRating}</div>
                             </div>
                             <div className="item">
                                 <div className="name">Category</div>
-                                <div className="value">C+</div>
-                            </div>                            
+                                <div className="value">{movie.movieCategory}</div>
+                            </div>
                         </div>
                     </div>
                     <div className="details-movie-description">
-                        Повече от десетилетие младите родители Анди (Зак Ефрон, Най-великият шоумен) и Вики (Сидни Лемън, Наследници) се укриват в отчаян опит да опазят дъщеря им Чарли (Райън Кийра Армстронг, Черната вдовица) от това да бъде заловена и подложена на безкрайни тестове, а безпрецедентните ѝ пирокинетични способности, да бъдат използвани за създаване на оръжие за масово унищожение. Анди е научил Чарли да потиска силата си, която се активира от гняв или болка, но когато момичето навършва 11 години, огънят става все по-труден и по-труден за контролиране. След инцидент, който разкрива локацията на семейството, мистериозен оперативен агент е изпратен по петите им, за да залови Чарли и да я сложи под контрол веднъж завинаги. Ще има ли Чарли силата да овладее огъня и да спаси семейство си?
+                        {movie.description}
                     </div>
                     <div className="details-movie-poster-wrapper">
                         <img className="details-movie-poster" src={movie.posterLink} alt="poster" />
