0b8e1d2ecd846a231f7676a8bba42fbd06a51758
Dimitar Stoimenov <dstoimen0@gmail.com>
2022-05-24 00:23:57 +0300

    up

--- src/components/CreateProjection/CreateProjection.js
+++ src/components/CreateProjection/CreateProjection.js
@@ -8,6 +8,8 @@ import { create } from '../../services/projectionService';
 const CreateProjection = () => {
     const [halls, setHalls] = useState([]);
     const [movies, setMovies] = useState([]);
+    const [regularPrice, setRegularPrice] = useState(0);
+    const [studentPrice, setStudentPrice] = useState(0);
 
     useEffect(() => {
         getAllMovies()
@@ -32,6 +34,12 @@ const CreateProjection = () => {
             })
     }
 
+    const hallChangeHandler = (e) => {
+        const targetHall = halls.find(x => x._id === e.target.value);
+        setRegularPrice(targetHall.regularPrice);
+        setStudentPrice(targetHall.studentPrice);
+    }
+
     return (
         <div className="create-container">
             <form method="POST" onSubmit={createHandler}>
@@ -39,14 +47,14 @@ const CreateProjection = () => {
                     <option value="no-value" disabled hidden>Select movie...</option>
                     {movies.map(movie => <option value={movie._id} key={movie._id}>{movie.movieName}</option>)}
                 </select>
-                <select className="form-input" defaultValue="no-value" name="hallId" id="hallId">
+                <select className="form-input" defaultValue="no-value" name="hallId" id="hallId" onChange={hallChangeHandler}>
                     <option value="no-value" disabled hidden >Select hall...</option>
                     {halls.map(hall => <option value={hall._id} key={hall._id}>{hall.hallName}</option>)}
                 </select>
                 <input type="text" className="form-input" name="date" placeholder="date - example 30/12/2022" />
                 <input type="text" className="form-input" name="hour" placeholder="hour - example 19:00" />
-                <input type="number" className="form-input" name="regularPrice" placeholder="regular price" />
-                <input type="number" className="form-input" name="studentsPrice" placeholder="students price" />
+                <input type="number" className="form-input" name="regularPrice" placeholder="regular price" value={regularPrice}/>
+                <input type="number" className="form-input" name="studentsPrice" placeholder="students price" value={studentPrice}/>
                 <button type="submit" className="submit-button">Create Projection</button>
             </form>
         </div>
