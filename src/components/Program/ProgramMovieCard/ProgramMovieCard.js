8ff17d4929eb3e044fb3dde8361ccf58a86944ae
Dimitar Stoimenov <dstoimen0@gmail.com>
2022-05-24 00:30:11 +0300

    up

--- src/components/Program/ProgramMovieCard/ProgramMovieCard.js
+++ src/components/Program/ProgramMovieCard/ProgramMovieCard.js
@@ -1,12 +1,19 @@
+import { useNavigate } from 'react-router-dom';
 import './ProgramMovieCard.css';
 
+
 const ProgramMovieCard = ({ movieId, projectionsArray }) => {
+    const navigate = useNavigate();
+    const onPosterClickHandler = () => {
+        navigate(`/movies/${movieId}`);
+        window.scrollTo(0, 0);
+    };
 
     return (
         <div className="program-movie-card">
             <div className="program-movie-tab">
                 <div className="program-movie-poster-wrapper">
-                    <img src={projectionsArray[0].movieId.posterLink} alt="poster" className="program-movie-poster" />
+                    <img src={projectionsArray[0].movieId.posterLink} alt="poster" className="program-movie-poster" onClick={onPosterClickHandler} />
                 </div>
                 <div className="program-movie-info">
 
